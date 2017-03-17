bd8a2c97cc238842349e3fa152105d54f3799aaf
Michel G. Souza <Michelgomesdes@hotmail.com>
2017-03-17 16:09:31 +0000

    Update forca.c

--- forca.c
+++ forca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 //Fazer forca.h
 int cont_palavra(char v[]){
 	int i = 0;
@@ -14,7 +15,7 @@ void s_palavra(char v[], char letra, char under[]){
 			under[i] = letra;
 		}
 	}
-	printf("\n");	
+	printf("\n");
 	//Ver em que casas as palavras estão;
 }
 
@@ -38,6 +39,17 @@ void imprimir(char v[]){
 	}
 }
 
+int verifica_under(char v[]){
+    int i =0;
+    for(i = 0;v[i] != '\0';i++){
+
+        if(v[i] == '_'){
+            return 1;
+        }
+    }
+
+}
+
 int main(){
 	char v[] = "abacaxi";
 	char under[30];
@@ -47,22 +59,22 @@ int main(){
  	inserir(under, cont_palavra(v));
 	imprimir(under);
 
- 	while(1){
+ 	while(verifica_under(under)){
 		printf("Digite uma Letra:\n");
 		scanf("%c", &a);
 		setbuf(stdin, NULL);
+
+        system("clear || cls");
 
 	 	s_palavra(v, a, under);
 		printf("\n");
 		imprimir(under);
 	}
  	/*desenha_under(cont_palavra(v),&under);
-
  	printf("V2: %c \n",under[0] );
 	/*scanf("%d", &a);
 	printf("Numero: %d \n",a );
-
 	printf("Palavra: %s\n", v );*/
-	
+
 return 0;
 }
