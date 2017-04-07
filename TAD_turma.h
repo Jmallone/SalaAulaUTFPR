cd7c96033e481fb81282438279444b0e40bfdbd9
Michel G. Souza <Michelgomesdes@hotmail.com>
2017-04-07 11:13:12 -0300

    Update TAD_turma.h

--- TAD_turma.h
+++ TAD_turma.h
@@ -1,65 +1,150 @@
-#ifndef TAD_TURMA_H_INCLUDED
-#define TAD_TURMA_H_INCLUDED
-#include "TAD_aluno.h"
+#ifndef TAD_ALUNO_H_INCLUDED
+#define TAD_ALUNO_H_INCLUDED
+#include <string.h>
 #include <stdlib.h>
 
 typedef struct{
-	char codigo[10];
-	Aluno** vetAlunos;
-	int tamVetor;
-	int matriculados;
-}Turma;
+	int ra;
+	char nome[50];
+	float* notas;
+	int qtdeNotas;  
+}Aluno;
 
 // DEFINA O PROTOTIPO E DESENVOLVA AS FUNCOES DESCRITAS A SEGUIR:
 
 
-// Criar uma turma
-Turma* criar_turma(char* codigo, int tamVetor ){
-	
-	Turma* t = (Turma*)malloc(sizeof(Turma));
-	t->matriculados = 0;
-	t->tamVetor = tamVetor;
-	
-	t->vetAlunos = (Aluno**)malloc(tamVetor*sizeof(Aluno*));
+// Criar um aluno
+Aluno* aluno_cria(int ra, char* nome,int qtdeNotas );
 
+// Destruir (desalocar o espaco previamente alocado) um aluno
+void aluno_libera(Aluno* aluno);
+
+// Obter a media do aluno
+float aluno_media(Aluno* aluno);
+
+// Obter o primeiro nome do aluno
+char* aluno_primeiro(Aluno* aluno);
+
+// Obter o ultimo nome do aluno
+char* aluno_segundo(Aluno* aluno);
+
+// Comparar dois alunos pelo nome
+//1 para igual
+// 0 para diferente
+int aluno_nomecompara(Aluno* aluno1, Aluno* aluno2);
+
+// Comparar dois alunos pela media
+int aluno_mediacompara(Aluno* aluno1, Aluno* aluno2);
+
+// IMPLEMENTE AS FUNCOES AQUI
+Aluno* aluno_cria(int ra, char* nome,int qtdeNotas ){
 	
+	Aluno* a = (Aluno*)malloc(sizeof(Aluno));
+	a->notas = (float*)malloc(qtdeNotas*(sizeof(float)));
 	
-	return t;
+	a->ra = ra;
+	strcpy(a->nome,nome);
+	a->qtdeNotas = qtdeNotas;
+	
+	return a;
+
 }
 
-// Destruir (desalocar o espaco previamente alocado) uma turma
-void deletar_turma(Turma* t){
-	int i = 0;
+void aluno_libera(Aluno* aluno){
 	
-	for(i = 0; i < t->matriculados;i++){
-		free(t->vetAlunos[i]->notas);
+	free(aluno->notas);
+	free(aluno);
+	
+}
+
+float aluno_media(Aluno* aluno){
+	float media = 0;
+	int i = 0;
+	for(i = 0; i<aluno->qtdeNotas; i++){
+
+		media = media+aluno->notas[i];
+	}
+	media = media/aluno->qtdeNotas;
+	return media;
+	
+}
+
+// Obter o primeiro nome do aluno
+char* aluno_primeiro(Aluno* aluno){
+	int i = 0;
+	char* primeiro = (char*)malloc(sizeof(char));
+	for(i =0; aluno->nome[i] != ' ' ;i++){
+		
+		primeiro[i] = aluno->nome[i];
+		
 		
 	}
-	free(t->vetAlunos);
-	free(t);
-	t->matriculados = 0;
+	primeiro[i] = '\0';
+	//printf("%s", primeiro);
 	
-	
+	return primeiro;
 	
 }
-// Matricular um aluno
 
-// Cancelar matricula de um aluno
+char* aluno_segundo(Aluno* aluno){
+	int i = 0;
+	for(i=0; aluno->nome[i] != ' ';i++);
+	i++;
+	char* segundo = (char*)malloc(sizeof(char));
+	int j = 0;
+	
+	while(aluno->nome[i] != '\0' && aluno->nome[i] != ' '){
+		
+		segundo[j] = aluno->nome[i];
+		i++;
+		j++;
+		
+	}
+	segundo[j] = '\0';
+	
+	return segundo;
+	
+}
 
-// Imprimir os alunos matriculados
+// Comparar dois alunos pelo nome
+//0 Diferente
+// 1 Igual
+int aluno_nomecompara(Aluno* aluno1, Aluno* aluno2){
+	
+	int i =0;
+	for(i= 0; aluno1->nome[i] != '\0' ; i++){
+			
+		if(aluno1->nome[i] != aluno2->nome[i]){
+			
+			return 0;
+			
+		}
+		
+	}
+	
+	return 1;
+	
+}
 
-// Obter a media da turma
+// Comparar dois alunos pela media
+// -1 Menor
+//0 - Igual
+//1 -maior
+int aluno_mediacompara(Aluno* aluno1, Aluno* aluno2){
+	
+	if(aluno_media(aluno1) > aluno_media(aluno2) ){
+		return 1;
+		
+	}else if(aluno_media(aluno1) < aluno_media(aluno2) ){
+		
+		return -1 ;
+		
+	}else{
+		
+		return 0;
+		
+	}
+	
+}
 
-// Verificar se um determinado aluno esta matriculado (RA)
-
-// Ordenar alunos por ordem alfabetica
-
-// Ordenar alunos por media
-
-
-
-
-
-
-
-#endif ///TAD_TURMA_H_INCLUDED
+#endif ///TAD_ALUNO_H_INCLUDED
